bfb4ec48d5431122d26db866d9efaa96a21d49be
CodyKank <cody.kankel@gmail.com>
2017-11-26 12:55:13 -0500

    Further changes to imagenet branch
    The image is saved now, I am currently attempting to figure out how to save an image in tiff format to be used in CIELAB format, it is not going well.

--- trial2.py
+++ trial2.py
@@ -6,6 +6,11 @@ import numpy as np
 import skimage
 from skimage import io, color 
 from keras.optimizers import SGD
+import glob
+
+
+# Loading all training images into a numpy array
+#GLob the directory, and load all images and do the following in a loop!
 
 
 # Loading an initial test image
@@ -34,7 +39,7 @@ bBand = np.array(labImage[:,:,2])
 desireBands[0,0:50176] = (aBand[0:224, 0:224]).flatten()/128.0
 desireBands[0,50176:] = (bBand[0:224,0:224]).flatten()/128.0
 
-sgd = SGD(lr=1e-4, momentum=1.9)
+sgd = SGD(lr=0.01, momentum=0.5)
 
 
 vgg16 = VGG16(weights='imagenet')
@@ -58,12 +63,18 @@ model.fit(x=L_input, y=desireBands, batch_size=1, epochs=100)
 
 testing = model.predict(x = L_input)
 
-testing.reshape((224,224,2))
+testing = testing.transpose()
+testingA = testing[0:50176,:].reshape((224,224))*127.0*127.0
+testingB = testing[50176:,:].reshape((224,224))*127.0*127.0
 
 testingFull = np.zeros((224,224,3))
 testingFull[:,:,0] = L_band[0:224,0:224]
-testingFull[:,:,1] = testing[:,:,0]
-testingFull[:,:,2] = testing[:,:,1]
+
+
+testingFull[:,:,1] = testingA
+testingFull[:,:,2] = testingB
+
+rgbsave = skimage.color.lab2rgb(testingFull)
 
 skimage.io.imsave('attempt1.jpg', skimage.color.lab2rgb(testingFull))
 
