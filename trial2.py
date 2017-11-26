802dcceadbc3a4cddedc61f118a8fa604f57e59d
CodyKank <cody.kankel@gmail.com>
2017-11-26 18:01:38 -0500

    Added generator like features to imagenet branch.
    There is a lot of cleaning up to do in this working piece of code, I wanted to push it so the group could see the entire thought process. I've commented out the possibly useless pieces of code to be removed once we can verify this is working. Currently, the code tests for 100 epochs and takes an eternity. Could maybe switch network models to one which trains faster, or find another way. I created a generator get_images which yields what the model expects for an input. Perhaps I am missing something but it doesn't work very well. It fails upon finding an image which does not have 3 bands of color, or at least that's how I interpreted the error message.

--- trial2.py
+++ trial2.py
@@ -12,19 +12,50 @@ import glob
 # Loading all training images into a numpy array
 #Glob the directory, and load all images and do the following in a loop!
 # (Num images, size, size, numBands)
-trainInput = np.zeros((60396, 224, 224, 3))
-trainTarget = np.zeros((60396, 100352))
-listCount = 0
+#trainInput = np.zeros((60396, 224, 224, 3))
+#trainTarget = np.zeros((60396, 100352))
+#listCount = 0
 # Grabbing all jpg images within train directory
-for filename in glob.glob('/data/mit1/images256/train/*.jpg'):
+"""This is set up in a way where each image will be denoted by the first dimension as desired by keras.
+Thereafter, the output data (labels) will be the flattened arrays of each desired output from the network."""
+"""for filename in glob.glob('/data/mit1/images256/train/*.jpg'):
     image = skimage.color.rgb2lab(skimage.io.imread(filename))
     trainInput[listCount, :, :, 0] = image[0:224,0:224,0] 
     trainTarget[listCount, 0:50176] = image[0:224,0:224,1] # A band ***May need to flatten() these?
-    trainTarget[listCount, 50176:] = image[0:224,0:224,2] # B band
+    trainTarget[listCount, 50176:] = image[0:224,0:224,2] # B band"""
 
+def get_images(path):
+    """ Generator to obtain and yield an image from a directory. YIELDS a tuple of an input band and desired bands (A + B) in a
+    flattened array
+    IN: A string denoting the full path to the images, with a wildcard ie /data/mit1/images256/meow/*.jpg"""
+    for filename in glob.glob(path):
+        image = skimage.color.rgb2lab(skimage.io.imread(filename))
+        Lband = np.zeros((1, 224,224,3))
+        Lband[0, :,:,0] = image[0:224,0:224,0]
+        trainTarget = np.zeros((1, 100352))
+        trainTarget[0, 0:50176] = np.array(image[0:224,0:224,1]).flatten() # A band ***May need to flatten() these?
+        trainTarget[0, 50176:] = np.array(image[0:224,0:224,2]).flatten() # B band
+        yield(Lband, trainTarget)
+
+"""
+listCount = 0
+validationIn = np.zeros((27894, 224, 224, 3))
+validationTarg = np.zeros((27894, 100352))
+validationData = (validationIn, validationData) # The tuple of these two
 # Need to repeat above for validation set
+for filename in glob.glob('/data/mit1/images256/validation/*.jpg'):
+    image = skimage.color.rgb2lab(skimage.io.imread(filename))
+    validationIn[listCount, :, :, 0] = image[0:224,0:224,0] 
+    validationTarg[listCount, 0:50176] = image[0:224,0:224,1] # A band ***May need to flatten() these?
+    validationTarg[listCount, 50176:] = image[0:224,0:224,2] # B band
+"""
+trainGen = get_images('/data/mit1/images256/train/*.jpg') # Creating generator for training images
+validationGen = get_images('/data/mit1/images256/validation/*.jpg') # Creating generator for validation images
+
+
 # Change model input to accept the new arrays of trainInput and trainTarget, set validation data as well.
 
+"""
 # Loading an initial test image
 image = skimage.io.imread('fireTruck.jpg')
 
@@ -50,11 +81,11 @@ bBand = np.array(labImage[:,:,2])
 #desireBands[65536:] = bBand
 desireBands[0,0:50176] = (aBand[0:224, 0:224]).flatten()/128.0
 desireBands[0,50176:] = (bBand[0:224,0:224]).flatten()/128.0
-
+"""
 sgd = SGD(lr=0.01, momentum=0.5)
 
 
-vgg16 = VGG16(weights='imagenet')
+vgg16 = VGG16(weights='imagenet', input_shape=(224,224,3))
 
 model = Sequential()
 model.add(vgg16)
@@ -71,7 +102,8 @@ model.summary()
 
 model.compile(optimizer=sgd, loss='mean_squared_error', metrics=['accuracy'])
 
-model.fit(x=L_input, y=desireBands, batch_size=1, epochs=100)
+#model.fit(x=trainInput, y=trainTarget, validation_data=validationData, batch_size=32, epochs=100)
+model.fit_generator(trainGen, validation_data = validationGen, steps_per_epoch = 60396, validation_steps= 27894, epochs=100)
 
 testing = model.predict(x = L_input)
 
