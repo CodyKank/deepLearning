c81bedfb4e02c93dab38b7634d2b75d1bfcd9ec8
CodyKank <cody.kankel@gmail.com>
2017-12-07 15:51:10 -0500

    Cleaned up code.
    Took out some comments that were useless.

--- modelTrain.py
+++ modelTrain.py
@@ -19,13 +19,6 @@ Colorizing a Gray Scale image utilizing LAB format.
 L band - Brightness, is the 0 band which will be passed into the network for an input. Range of values: [0,100]
 A and B band - Deal with colors. Range of values: [-128, 128] (give or take)
 
-Operating the network: This network is thus far only created to output a correct shape. There are no weights associated
-yet. The plan is to train the network to properly adjust the weights to make the output shape the A and B bands.
-
-Training on A and B bands: Getting the right output shape is a pain. The current shape is (75,75, 6). This means the A band
-will be placed witin (75, 75, 0) through (75,75,2) and B will be placed in the res (3-5). This way, both bands can be 
-discovered and trained for at once.
-
 Note on training: The A and B bands will have to all be divided by 128 to make their values fall between [-1, and 1]. This is so
 the output of the network can be mapped to the correct values, as it outputs [-1,1] so the output values can be multiplied by
 128 to create the desired values (in theory).
@@ -37,50 +30,10 @@ def main():
 
     #trainGen = get_Arrayimages('/data/mit1/images256/p/newPastures/*.jpg') # Creating generator for training images
     #validationGen = get_Arrayimages('/data/mit1/images256/p/validation/*.jpg') # Creating generator for validation images
+    #trainGen = get_images('/data/mit1/images256/p/newPastures/*.jpg') # Creating generator for training images
     trainGen = get_images('/data/mit1/images256/p/newPastures/*.jpg') # Creating generator for training images
     validationGen = get_images('/data/mit1/images256/tempTrain/*jpg') # Creating generator for validation images
 
-    """
-    # Loading an initial test image
-    image = skimage.io.imread('.jpg')
-
-    # Moving the original image into LAB format
-    labImage = skimage.color.rgb2lab((image))
-    #labImage.reshape(225,225,3)
-
-    # Grabbing just the L band (brightness)
-    L_band = labImage[:,:,0]
-
-    # Moving the L_band list into an np array
-    L_input = np.zeros((1, 225,225,1))
-    L_input[0,:,:,0] = L_band[0:225,0:225]
-
-    #L_input = np.array(L_band, dtype=float)
-
-    #image = np.array(image, dtype=float)
-    Desired_Band_A = np.array(labImage[0:225,0:225,1], dtype=float)/128.0
-    Desired_Band_B = np.array(labImage[0:225,0:225,2], dtype=float)/128.0
-
-    # For the labels, must split 225x225 into 1250 9x9 blocks !!!
-    imageLabel = np.zeros((1,9,9,1250))
-
-    #Splitting bands A and B into 3 equal sized arrays len=75
-    A_List = []
-
-
-    # Splitting each band into 625 9x9 blocks
-    A_List = splitArray(Desired_Band_A)
-    B_List = splitArray(Desired_Band_B)
-    
-    # Going through each list and creating a 75,75,6 for the two bands
-    for i in range(0,1250):
-        if i < 625:
-            imageLabel[0,:,:,i] = np.array(A_List[i], dtype=float)
-        else:
-            imageLabel[0,:,:,i] = np.array(B_List[i-625],dtype=float)
-
-
-    """
     model = Sequential()
     model.add(Convolution2D(8, (3, 3), activation='relu', padding='same', strides=2,input_shape=(225, 225,1)))
     model.add(Convolution2D(8, (3, 3), activation='relu', padding='same'))
@@ -106,53 +59,16 @@ def main():
 
     model.add(Convolution2D(1250,(9,9), activation='tanh'))
     #model.compile(optimizer='rmsprop', loss='mse')
-    sgd = SGD(lr=0.01, momentum=0.2)
+    sgd = SGD(lr=0.04, momentum=0.4)
     model.compile(optimizer=sgd, loss='mean_squared_error', metrics=['accuracy'])
 
     model.summary()
     print( model.output_shape)
     
     #model.fit_generator(trainGen, validation_data = validationGen, steps_per_epoch = 100, validation_steps=10, epochs=5)
-    model.fit_generator(trainGen, steps_per_epoch =594, epochs=2)
+    model.fit_generator(trainGen, steps_per_epoch = 1181, epochs=2)
     model.save('deepCorloziationWeights.h5')
     #model.fit(x=L_input, y=imageLabel, batch_size=1, epochs=200)
-    """
-    
-    # Testing the image we just trained on
-    testing = model.predict(L_input)
-    testing[0,:,:,:] *= 128.0
-    resA = mergeArray(testing[0,:,:,0:625])
-    resB = mergeArray(testing[0,:,:,625:])
-
-    resA[0:75, 0:75] = (testing[0,:,:,0])
-    resA[0:75, 75:150] = (testing[0,:,:,1])
-    resA[0:75, 150:225] = testing[0,:,:,2]
-    resA[75:150, 0:75] = testing[0,:,:,3]
-    resA[75:150, 150:225] = testing[0,:,:,4]
-    resA[75:150, 150:225] = testing[0,:,:,5]
-    resA[150:225, 0:75] = testing[0,:,:,6]
-    resA[150:225, 75:150] = testing[0,:,:,7]
-    resA[150:225, 150:225] = testing[0,:,:,8]
-
-    resB[0:75, 0:75] = testing[0,:,:,9]
-    resB[0:75, 75:150] = testing[0,:,:,10]
-    resB[0:75, 150:225] = testing[0,:,:,11]
-    resB[75:150, 0:75] = testing[0,:,:,12]
-    resB[75:150, 150:225] = testing[0,:,:,13]
-    resB[75:150, 150:225] = testing[0,:,:,14]
-    resB[150:225, 0:75] = testing[0,:,:,15]
-    resB[150:225, 75:150] = testing[0,:,:,16]
-    resB[150:225, 150:225] = testing[0,:,:,17]
-    
-
-    trialImage = np.zeros((225,225,3))
-    trialImage[:,:,0] = L_band[0:225, 0:225]
-    trialImage[:,:,1] = resA
-    trialImage[:,:,2] = resB
-    
-    saveImage = skimage.color.lab2rgb(trialImage)
-    skimage.io.imsave('meow.jpg', saveImage)
-    """
 
 #^-----------------------------------------------------------------------------main()
     
